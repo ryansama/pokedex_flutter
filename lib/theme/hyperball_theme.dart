f4e5dcd1daa52963c8b33839efaadda38b4398b7
Ryan Samarajeewa <ryan.samarajeewa@gmail.com>
2019-06-25 19:22:57 -0700

    Add theme

---
+++ lib/theme/hyperball_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+ThemeData hyperBallTheme() {
+  return ThemeData(
+    primaryColor: Colors.blueGrey,
+    accentColor: Colors.orangeAccent,
+    hintColor: Colors.grey,
+    dividerColor: Colors.grey,
+    buttonColor: Colors.grey,
+    scaffoldBackgroundColor: Colors.white,
+    canvasColor: Colors.black,
+  );
+}
