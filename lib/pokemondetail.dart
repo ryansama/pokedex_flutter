5807eeb5aecad22dd9f37e2bf539f495662395e9
Ryan Samarajeewa <ryan.samarajeewa@gmail.com>
2019-06-28 19:21:06 -0700

    Pokemon details page partial commit

--- lib/pokemondetail.dart
+++ lib/pokemondetail.dart
@@ -18,6 +18,8 @@ class _PokemonDetailState extends State<PokemonDetail> {
   Future<Pokemon> pokemon;
   final gateway = new GraphQLGateway();
 
+  final double IMAGE_HEIGHT = 200.0;
+
   _PokemonDetailState(this.id);
 
   @override
@@ -48,53 +50,67 @@ class _PokemonDetailState extends State<PokemonDetail> {
 
   Widget _buildDetailsView(Pokemon p) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        child: Stack(
-          children: <Widget>[
-            Container(
-              child: _buildBanner(p),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: SafeArea(
+            child: CustomScrollView(
+              slivers: <Widget>[
+                _buildSliverAppBar(p),
+                SliverFillRemaining(child: _buildDetailCards(p))
+              ],
             ),
-            new Positioned(
-              //Place it at the top, and not use the entire screen
-              top: 0.0,
-              left: 0.0,
-              right: 0.0,
-              child: AppBar(
-                automaticallyImplyLeading: true,
-                //`true` if you want Flutter to automatically add Back Button when needed,
-                //or `false` if you want to force your own back button every where
-                leading: IconButton(
-                  icon: Icon(Icons.arrow_back),
-                  color: Colors.black,
-                  onPressed: () => Navigator.pop(context, false),
-                ),
-                backgroundColor: Colors.transparent, //No more green
-                elevation: 0.0, //Shadow gone
-              ),
-            ),
-          ],
+          ),
+        ));
+  }
+
+  SliverAppBar _buildSliverAppBar(Pokemon p) {
+    return SliverAppBar(
+      automaticallyImplyLeading: true,
+      leading: IconButton(
+        icon: Icon(Icons.arrow_back),
+        color: Colors.black,
+        onPressed: () => Navigator.pop(context, false),
+      ),
+      backgroundColor: Colors.white,
+      pinned: true,
+      expandedHeight: IMAGE_HEIGHT,
+      flexibleSpace: FlexibleSpaceBar(
+        title: Text(
+          p.name,
+          style: TextStyle(color: Colors.black),
         ),
+        background: Stack(children: <Widget>[
+          Center(
+              child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: _buildBannerImage(p),
+          )),
+          Container(
+            height: 350.0,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                gradient: LinearGradient(
+                    begin: FractionalOffset.topCenter,
+                    end: FractionalOffset.bottomCenter,
+                    colors: [
+                      Colors.grey.withOpacity(0.0),
+                      Colors.white54,
+                    ],
+                    stops: [
+                      0.0,
+                      1.0
+                    ])),
+          )
+        ]),
       ),
     );
   }
 
-  Scaffold _buildBanner(Pokemon p) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            height: 250,
-            child: FadeInImage.memoryNetwork(
-              placeholder: kTransparentImage,
-              image:
-                  'https://img.pokemondb.net/artwork/${getUrlFriendlyName(p.name)}.jpg',
-            ),
-          ),
-        ],
-      ),
+  FadeInImage _buildBannerImage(Pokemon p) {
+    return FadeInImage.memoryNetwork(
+      placeholder: kTransparentImage,
+      image:
+          'https://img.pokemondb.net/artwork/${getUrlFriendlyName(p.name)}.jpg',
     );
   }
 
@@ -103,4 +119,77 @@ class _PokemonDetailState extends State<PokemonDetail> {
       .replaceAll(".", "-")
       .replaceAll(" ", "")
       .toLowerCase();
+
+  _buildDetailCards(Pokemon p) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+            child: _buildTopRow(p),
+          )
+        ],
+      ),
+    );
+  }
+
+  IntrinsicHeight _buildTopRow(Pokemon p) {
+    return IntrinsicHeight(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  color: Colors.transparent,
+                  height: 50.0,
+                  width: 0.0,
+                ),
+                Container(
+                  height: 50.0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(children: <Widget>[
+                      Text(p.weight.maximum, style: TextStyle(fontWeight: FontWeight.bold),),
+                      Text("Max wt", style: TextStyle(color: Colors.grey),)
+                    ],),
+                  ),
+                ),
+                Container(
+                  color: Colors.black12,
+                  height: 50.0,
+                  width: 1.0,
+                ),
+                Container(
+                  height: 50.0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(children: <Widget>[
+                      Text(p.height.maximum, style: TextStyle(fontWeight: FontWeight.bold),),
+                      Text("Max ht", style: TextStyle(color: Colors.grey),)
+                    ],),
+                  ),
+                ),
+                Container(
+                  color: Colors.black12,
+                  height: 50.0,
+                  width: 1.0,
+                ),
+                Container(
+                  height: 50.0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(children: <Widget>[
+                      Text(p.maxHP.toString(), style: TextStyle(fontWeight: FontWeight.bold),),
+                      Text("Max HP", style: TextStyle(color: Colors.grey),)
+                    ],),
+                  ),
+                ),
+                Container(
+                  color: Colors.black12,
+                  height: 50.0,
+                  width: 0.0,
+                ),
+              ],
+            ),
+          );
+  }
 }
