b7b18abaa7f8900f564431a7610cd6e2f976d7ba
Ryan Samarajeewa <ryan.samarajeewa@gmail.com>
2019-07-01 23:05:50 -0700

    Add Attacks card

--- lib/pokemondetail.dart
+++ lib/pokemondetail.dart
@@ -133,7 +133,8 @@ class _PokemonDetailState extends State<PokemonDetail> {
             _buildBasicInfoRow(p),
             _buildTypesRow(p),
             _buildEvolutionRow(p),
-            _buildStrongWeakRow(p)
+            _buildStrongWeakRow(p),
+            _buildMovesRow(p)
           ],
         ),
       ),
@@ -144,12 +145,12 @@ class _PokemonDetailState extends State<PokemonDetail> {
     final TextTheme textTheme = Theme.of(context).textTheme;
     var evolutionRow = List<Widget>();
     if (p.prevEvolutions != null) {
-      for (int i = 0; i < p.prevEvolutions.length; i++) {
+      for (var pe in p.prevEvolutions) {
         evolutionRow.add(CircleAvatar(
             backgroundColor: Colors.transparent,
             child: CachedNetworkImage(
               imageUrl:
-                  "https://img.pokemondb.net/sprites/sun-moon/icon/${getUrlFriendlyName(p.prevEvolutions[i].name)}.png",
+                  "https://img.pokemondb.net/sprites/sun-moon/icon/${getUrlFriendlyName(pe.name)}.png",
             )));
 
         evolutionRow
@@ -165,7 +166,7 @@ class _PokemonDetailState extends State<PokemonDetail> {
         )));
 
     if (p.evolutions != null) {
-      for (int i = 0; i < p.evolutions.length; i++) {
+      for (var e in p.evolutions) {
         evolutionRow
             .add(Icon(Icons.arrow_forward_ios, color: Color(0xff424242)));
 
@@ -173,7 +174,7 @@ class _PokemonDetailState extends State<PokemonDetail> {
             backgroundColor: Colors.transparent,
             child: CachedNetworkImage(
               imageUrl:
-                  "https://img.pokemondb.net/sprites/sun-moon/icon/${getUrlFriendlyName(p.evolutions[i].name)}.png",
+                  "https://img.pokemondb.net/sprites/sun-moon/icon/${getUrlFriendlyName(e.name)}.png",
             )));
       }
     }
@@ -181,13 +182,20 @@ class _PokemonDetailState extends State<PokemonDetail> {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Card(
+        elevation: 2,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
         child: Padding(
           padding: const EdgeInsets.all(12.0),
           child: Container(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text("Evolution", style: textTheme.headline),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text("Evolution", style: textTheme.headline),
+                ),
                 Padding(
                   padding: const EdgeInsets.all(12.0),
                   child: Row(
@@ -348,39 +356,42 @@ class _PokemonDetailState extends State<PokemonDetail> {
       ),
     );
   }
-}
 
-_buildStrongWeakRow(Pokemon p) {
-  List<Widget> resistantTypes = new List<Widget>();
-  List<Widget> weakTypes = new List<Widget>();
+  _buildStrongWeakRow(Pokemon p) {
+    List<Widget> resistantTypes = new List<Widget>();
+    List<Widget> weakTypes = new List<Widget>();
 
-  resistantTypes.add(Padding(
-    padding: const EdgeInsets.only(bottom: 8.0),
-    child: Text("Resists", style: TextStyle(color: Colors.white, fontSize: 20)),
-  ));
+    resistantTypes.add(Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child:
+          Text("Resists", style: TextStyle(color: Colors.white, fontSize: 20)),
+    ));
 
-  weakTypes.add(Padding(
-    padding: const EdgeInsets.only(bottom: 8.0),
-    child:
-        Text("Weaknesses", style: TextStyle(color: Colors.white, fontSize: 20)),
-  ));
+    weakTypes.add(Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Text("Weaknesses",
+          style: TextStyle(color: Colors.white, fontSize: 20)),
+    ));
 
-  for (int i = 0; i < p.resistant.length; i++) {
-    resistantTypes.add(_buildTypeCard(p.resistant[i]));
-  }
+    for (var r in p.resistant) {
+      resistantTypes.add(_buildTypeCard(r));
+    }
 
-  for (int i = 0; i < p.weaknesses.length; i++) {
-    weakTypes.add(_buildTypeCard(p.weaknesses[i]));
-  }
+    for (var w in p.weaknesses) {
+      weakTypes.add(_buildTypeCard(w));
+    }
 
-  return Padding(
-    padding: const EdgeInsets.all(8.0),
-    child: IntrinsicHeight(
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
       child: Row(
         children: <Widget>[
           Expanded(
             flex: 1,
             child: Card(
+              elevation: 2,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
               color: Colors.green[400],
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -391,6 +402,10 @@ _buildStrongWeakRow(Pokemon p) {
           Expanded(
             flex: 1,
             child: Card(
+              elevation: 2,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
               color: Colors.red[400],
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -400,29 +415,109 @@ _buildStrongWeakRow(Pokemon p) {
           )
         ],
       ),
-    ),
-  );
-}
+    );
+  }
 
-_buildTypeCard(type) {
-  return Card(
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(15.0),
-          bottomLeft: Radius.circular(15.0),
-          topRight: Radius.circular(15.0),
-          bottomRight: Radius.circular(15.0)),
-    ),
-    child: Container(
-      width: 100,
-      height: 30,
-      child: Center(
-        child: Text(type,
-            style: TextStyle(
-                color: Colors.white,
-                shadows: <Shadow>[Shadow(blurRadius: 2.0)])),
+  _buildTypeCard(type) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(15.0),
+            bottomLeft: Radius.circular(15.0),
+            topRight: Radius.circular(15.0),
+            bottomRight: Radius.circular(15.0)),
       ),
-    ),
-    color: typeColorMap[type],
-  );
+      child: Container(
+        width: 100,
+        height: 30,
+        child: Center(
+          child: Text(type,
+              style: TextStyle(
+                  color: Colors.white,
+                  shadows: <Shadow>[Shadow(blurRadius: 2.0)])),
+        ),
+      ),
+      color: typeColorMap[type],
+    );
+  }
+
+  _buildMovesRow(Pokemon p) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    List<Fast> fastMoves = p.attacks.fast;
+    List<Special> specialMoves = p.attacks.special;
+    var moveRows = List<Widget>();
+
+    moveRows.add(Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text("Attacks", style: textTheme.headline),
+    ));
+
+    for (var fm in fastMoves) {
+      var moveRow = new List<Widget>();
+
+      moveRow.add(Padding(
+        padding: const EdgeInsets.only(left: 8.0, bottom: 6.0, right: 6.0),
+        child: Text(fm.name, style: textTheme.body2),
+      ));
+
+      var numPills = fm.damage / 10;
+      for (int i = 0; i < numPills; i++) {
+        moveRow.add(_buildAttackPowerPill(fm));
+      }
+      moveRows.add(Row(children: moveRow));
+    }
+
+    for (var sm in specialMoves) {
+      var moveRow = new List<Widget>();
+
+      moveRow.add(Padding(
+        padding: const EdgeInsets.only(left: 8.0, bottom: 6.0, right: 6.0),
+        child: Text(sm.name, style: textTheme.body2),
+      ));
+
+      var numPills = sm.damage / 10;
+      for (int i = 0; i < 8; i++) {
+        moveRow.add(_buildAttackPowerPill(sm));
+      }
+      moveRows.add(Row(children: moveRow));
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        elevation: 2,
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Container(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: moveRows),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding _buildAttackPowerPill(fm) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 6.0),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(15.0),
+              bottomLeft: Radius.circular(15.0),
+              topRight: Radius.circular(15.0),
+              bottomRight: Radius.circular(15.0)),
+        ),
+        child: Container(
+          width: 20,
+          height: 10,
+        ),
+        color: typeColorMap[fm.type],
+      ),
+    );
+  }
 }
